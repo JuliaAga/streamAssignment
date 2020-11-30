8a731e52efc19e53f6e137d9da56629bec214db5
Yuliia <dongro@yandex.ru>
2020-11-30 20:07:56 +0500

    Clean solve task1

--- src/tasks/Task1.java
+++ src/tasks/Task1.java
@@ -4,11 +4,10 @@ import common.Person;
 import common.PersonService;
 import common.Task;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+
+import static java.util.function.Function.identity;
 
 /*
 Задача 1
@@ -17,27 +16,34 @@ import java.util.stream.Collectors;
 нужно их отсортировать в том же порядке, что и переданные id.
 Оценить асимпотику работы
  */
+
+/*
+Первый  прямолинейный вариант был: создать лист, и перебором по personsIds искать в persons
+нужного и добавлять в лист. Но в таком случае для каждого id в худшем случае перебирался бы весь set (т.к. ищем по
+полю в объекте).
+Второй вариант (финальный): создать hashMap (id, персона), перебором по personsId за время O(n) получать из map нужного
+за время O(1).
+*/
+
 public class Task1 implements Task {
 
-  // !!! Редактируйте этот метод !!!
-  private List<Person> findOrderedPersons(List<Integer> personIds) {
-    Set<Person> persons = PersonService.findPersons(personIds);
+    // !!! Редактируйте этот метод !!!
+    private List<Person> findOrderedPersons(List<Integer> personIds) {
+        Set<Person> persons = PersonService.findPersons(personIds);
+        Map<Integer, Person> personsMap = persons.stream().collect(Collectors.toMap(Person::getId, identity()));
+        List<Person> personsOutput =
+                personIds.stream().map(id -> personsMap.get(id)).collect(Collectors.toList());
+        return personsOutput;
+    }
 
-    List<Person> personsList = new ArrayList<>();
-    personIds.forEach(id -> {
-      personsList.add(persons.stream().filter(person -> person.getId() == id).findFirst().get());
-    });
-      return personsList;
-  }
+    @Override
+    public boolean check() {
+        List<Integer> ids = List.of(1, 2, 3);
 
-  @Override
-  public boolean check() {
-    List<Integer> ids = List.of(1, 2, 3);
-
-    return findOrderedPersons(ids).stream()
-        .map(Person::getId)
-        .collect(Collectors.toList())
-        .equals(ids);
-  }
+        return findOrderedPersons(ids).stream()
+                .map(Person::getId)
+                .collect(Collectors.toList())
+                .equals(ids);
+    }
 
 }
